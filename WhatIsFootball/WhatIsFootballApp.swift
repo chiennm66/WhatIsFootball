117dc66907c9024cb36c9ff4e5d1e2135f2a42d3
chienabc <chien97666@gmail.com>
2025-04-17 01:26:25 +0700

    Initial Commit

---
+++ WhatIsFootball/WhatIsFootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIsFootballApp.swift
+//  WhatIsFootball
+//
+//  Created by CHIEN on 17/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatIsFootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
